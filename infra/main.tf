88419da633735b8e8f1a137af3e21fe80dc6e3bc
TavaresASantos <tavaresamandasantos@gmail.com>
2026-05-18 08:36:21 -0300

    fix: correct policy ARN format for ECS execution role attachment

--- infra/main.tf
+++ infra/main.tf
@@ -188,7 +188,7 @@ resource "aws_iam_role" "ecs_execution_role" {
 
 resource "aws_iam_role_policy_attachment" "ecs_execution" {
   role       = aws_iam_role.ecs_execution_role.name
-  policy_arn = "arn:aws:iam:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 # CLOUDWATCH LOG GROUP FOR ECS
